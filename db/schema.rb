9eb1303affe130d390d710610dc321acc283f948
Brian Vuu <brianvuu@gmail.com>
2018-08-29 19:20:07 -0600

    added Item Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_004501) do
+ActiveRecord::Schema.define(version: 2018_08_30_010943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.boolean "in_stock"
+    t.bigint "lists_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lists_id"], name: "index_items_on_lists_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -32,5 +42,6 @@ ActiveRecord::Schema.define(version: 2018_08_30_004501) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "lists", column: "lists_id"
   add_foreign_key "lists", "stores"
 end
