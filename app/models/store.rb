08395d431b99d61760825a299a4538fab08ff45f
Brian Vuu <brianvuu@gmail.com>
2018-08-29 20:54:15 -0600

    added validation to store.rb

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,8 +1,20 @@
 class Store < ApplicationRecord
     #validations
-    # validates :name, presence: true
+    validates :name, presence: true
+
     #associations
     has_many :lists, dependent: :destroy
+    
     #callbacks
-    #Instance / Class methods 
+    
+    # Instance / Class methods 
+
+
+    # before_create :put_item_in_stock
+
+    # private
+    
+    # def find_store_by_name |x|
+    #     where name = x
+    # end
 end
